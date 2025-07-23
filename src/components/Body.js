fb3192e57a6692ba0dfbbaccd875c86d47cff247
anshu-raj-6041 <anshu604125@gmail.com>
2025-07-23 14:02:51 +0530

    login

--- src/components/Body.js
+++ src/components/Body.js
@@ -8,6 +8,9 @@ const Body = () => {
     // local State variable => super powerful variable
     const [listOfRestaurants, setListOfRestaurant] = useState([]);
 
+    // local state variable
+    const [searchText, setSearchText] = useState("");
+
     useEffect(() => {
         // console.log("useEffect called");
         fetchData();
@@ -19,102 +22,117 @@ const Body = () => {
         const json = await data.json();
 
         console.log(json);
-        setListOfRestaurant(json?.data?.cards[2]?.card?.card?.info)
+        // Optional Chaining
+        setListOfRestaurant(json?.data?.cards[5]?.card?.card?.info)
 
 
 
     };
 
-    if (listOfRestaurants.length == 0) {
-        return <Shimmer />;
-    }
+    // Conditional Rendering => Rendering based on the condition
+    return listOfRestaurants.length != 0 ? (
+        <Shimmer />
+    ) : (
 
 
-    // Normal JS variable
-    // let listOfRestaurantsJS = [
-    //     {
-    //         "card": {
-    //             "card": {
-    //                 "@type": "type.googleapis.com/swiggy.presentation.food.v2.Restaurant",
-    //                 "info": {
-    //                     "id": "479796",
-    //                     "name": "BBK",
-    //                     "cloudinaryImageId": "fl0bvksh5ujstl8ny1hv",
-    //                     "locality": "Kumharar",
-    //                     "areaName": "Kumhrar",
-    //                     "costForTwo": "₹400 for two",
-    //                     "cuisines": [
-    //                         "Sweets",
-    //                         "Bakery",
-    //                         "North Indian"
-    //                     ],
-    //                     "avgRating": 2.4,
-    //                     "veg": true,
-    //                     "parentId": "472621",
-    //                     "avgRatingString": "4.4",
-    //                     "totalRatingsString": "10K+",
-    //                     "promoted": true,
-    //                     "adTrackingId": "cid=31266486~p=0~adgrpid=31266486#ag1~mp=SWIGGY_IN~bl=FOOD~aet=RESTAURANT~aeid=479797~plpr=COLLECTION~eid=c909e686-e253-4f16-81c7-62b1e7689187~srvts=1752991874723~collid=80463",
-    //                     "sla": {
-    //                         "deliveryTime": 49,
-    //                         "lastMileTravel": 4.2,
-    //                         "serviceability": "SERVICEABLE",
-    //                         "slaString": "45-50 mins",
-    //                         "lastMileTravelString": "4.2 km",
-    //                         "iconType": "ICON_TYPE_EMPTY"
-    //                     },
-    //                     "availability": {
-    //                         "nextCloseTime": "2025-07-20 23:59:00",
-    //                         "opened": true
-    //                     },
-    //                 }
-    //             }
-    //         }
-    //     },
-    //     {
-    //         "card": {
-    //             "card": {
-    //                 "@type": "type.googleapis.com/swiggy.presentation.food.v2.Restaurant",
-    //                 "info": {
-    //                     "id": "479797",
-    //                     "name": "Harilal's",
-    //                     "cloudinaryImageId": "fl0bvksh5ujstl8ny1hv",
-    //                     "locality": "Kumharar",
-    //                     "areaName": "Kumhrar",
-    //                     "costForTwo": "₹400 for two",
-    //                     "cuisines": [
-    //                         "Sweets",
-    //                         "Bakery",
-    //                         "North Indian"
-    //                     ],
-    //                     "avgRating": 4.4,
-    //                     "veg": true,
-    //                     "parentId": "472621",
-    //                     "avgRatingString": "4.4",
-    //                     "totalRatingsString": "10K+",
-    //                     "promoted": true,
-    //                     "adTrackingId": "cid=31266486~p=0~adgrpid=31266486#ag1~mp=SWIGGY_IN~bl=FOOD~aet=RESTAURANT~aeid=479797~plpr=COLLECTION~eid=c909e686-e253-4f16-81c7-62b1e7689187~srvts=1752991874723~collid=80463",
-    //                     "sla": {
-    //                         "deliveryTime": 49,
-    //                         "lastMileTravel": 4.2,
-    //                         "serviceability": "SERVICEABLE",
-    //                         "slaString": "45-50 mins",
-    //                         "lastMileTravelString": "4.2 km",
-    //                         "iconType": "ICON_TYPE_EMPTY"
-    //                     },
-    //                     "availability": {
-    //                         "nextCloseTime": "2025-07-20 23:59:00",
-    //                         "opened": true
-    //                     },
-    //                 }
-    //             }
-    //         }
-    //     }
+        // Normal JS variable
+        // let listOfRestaurantsJS = [
+        //     {
+        //         "card": {
+        //             "card": {
+        //                 "@type": "type.googleapis.com/swiggy.presentation.food.v2.Restaurant",
+        //                 "info": {
+        //                     "id": "479796",
+        //                     "name": "BBK",
+        //                     "cloudinaryImageId": "fl0bvksh5ujstl8ny1hv",
+        //                     "locality": "Kumharar",
+        //                     "areaName": "Kumhrar",
+        //                     "costForTwo": "₹400 for two",
+        //                     "cuisines": [
+        //                         "Sweets",
+        //                         "Bakery",
+        //                         "North Indian"
+        //                     ],
+        //                     "avgRating": 2.4,
+        //                     "veg": true,
+        //                     "parentId": "472621",
+        //                     "avgRatingString": "4.4",
+        //                     "totalRatingsString": "10K+",
+        //                     "promoted": true,
+        //                     "adTrackingId": "cid=31266486~p=0~adgrpid=31266486#ag1~mp=SWIGGY_IN~bl=FOOD~aet=RESTAURANT~aeid=479797~plpr=COLLECTION~eid=c909e686-e253-4f16-81c7-62b1e7689187~srvts=1752991874723~collid=80463",
+        //                     "sla": {
+        //                         "deliveryTime": 49,
+        //                         "lastMileTravel": 4.2,
+        //                         "serviceability": "SERVICEABLE",
+        //                         "slaString": "45-50 mins",
+        //                         "lastMileTravelString": "4.2 km",
+        //                         "iconType": "ICON_TYPE_EMPTY"
+        //                     },
+        //                     "availability": {
+        //                         "nextCloseTime": "2025-07-20 23:59:00",
+        //                         "opened": true
+        //                     },
+        //                 }
+        //             }
+        //         }
+        //     },
+        //     {
+        //         "card": {
+        //             "card": {
+        //                 "@type": "type.googleapis.com/swiggy.presentation.food.v2.Restaurant",
+        //                 "info": {
+        //                     "id": "479797",
+        //                     "name": "Harilal's",
+        //                     "cloudinaryImageId": "fl0bvksh5ujstl8ny1hv",
+        //                     "locality": "Kumharar",
+        //                     "areaName": "Kumhrar",
+        //                     "costForTwo": "₹400 for two",
+        //                     "cuisines": [
+        //                         "Sweets",
+        //                         "Bakery",
+        //                         "North Indian"
+        //                     ],
+        //                     "avgRating": 4.4,
+        //                     "veg": true,
+        //                     "parentId": "472621",
+        //                     "avgRatingString": "4.4",
+        //                     "totalRatingsString": "10K+",
+        //                     "promoted": true,
+        //                     "adTrackingId": "cid=31266486~p=0~adgrpid=31266486#ag1~mp=SWIGGY_IN~bl=FOOD~aet=RESTAURANT~aeid=479797~plpr=COLLECTION~eid=c909e686-e253-4f16-81c7-62b1e7689187~srvts=1752991874723~collid=80463",
+        //                     "sla": {
+        //                         "deliveryTime": 49,
+        //                         "lastMileTravel": 4.2,
+        //                         "serviceability": "SERVICEABLE",
+        //                         "slaString": "45-50 mins",
+        //                         "lastMileTravelString": "4.2 km",
+        //                         "iconType": "ICON_TYPE_EMPTY"
+        //                     },
+        //                     "availability": {
+        //                         "nextCloseTime": "2025-07-20 23:59:00",
+        //                         "opened": true
+        //                     },
+        //                 }
+        //             }
+        //         }
+        //     }
 
-    // ]
-    return (
+        // ]
         <div className="body">
             <div className="filter">
+                <div className="search">
+                    <input type="text" className="search-box" value={searchText} 
+                    onChange={(e) => {
+                        setSearchText(e.target.value);
+                    }}/>
+                    <button onClick={() => {
+                        // Filter the res Card and update the UI
+                        // searchText
+                        console.log(searchText);
+
+                        const filteredRes = listOfRestaurants.filter((res) => res.data.name == searchText);
+                        
+                    }}>Search</button>
+                </div>
                 <button
                     className="filter-btn"
                     onClick={() => {
